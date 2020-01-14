74260973eed51f78a88e0c30ed088afe093df260
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-01-14 17:42:33 +0100

    basic env stuff

--- typing.pl
+++ typing.pl
@@ -1,4 +1,6 @@
-get_env(ENV, X) :- set_env(ENV, X, _).
-type(var(X), T, ENV) :- =(T, get_env(ENV, X)).
-type(app(F, A), T, ENV) :- type(F, [func, X, T], ENV), type(A, X, ENV).
-type(lambda(X, E), T, ENV) :- =(T, [func, A, B]), type(E, A, ENV), set_env(ENV, X, B).
+library(assoc).
+env_add(OLDENV, NEWENV, X, T) :- put_assoc(X, OLDENV, T, NEWENV).
+env_contains(ENV, X, T) :- get_assoc(X, ENV, T).
+type(var(X), T, ENV, _) :- env_contains(ENV, X, T).
+type(app(F, A), T, ENV, NE) :- type(F, [func, X, T], ENV, NE1), type(A, X, NE1, NE).
+type(lambda(X, E), T, ENV, NE) :- =(T, [func, A, B]), type(E, A, ENV, NE1), env_add(NE1, NE, X, B).
