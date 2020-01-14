286fa32254c5685d3c8c9a50af47fa3ff3685b9d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-01-14 17:50:25 +0100

    Make lambdas work

--- typing.pl
+++ typing.pl
@@ -3,4 +3,4 @@ env_add(OLDENV, NEWENV, X, T) :- put_assoc(X, OLDENV, T, NEWENV).
 env_contains(ENV, X, T) :- get_assoc(X, ENV, T).
 type(var(X), T, ENV, _) :- env_contains(ENV, X, T).
 type(app(F, A), T, ENV, NE) :- type(F, [func, X, T], ENV, NE1), type(A, X, NE1, NE).
-type(lambda(X, E), T, ENV, NE) :- =(T, [func, A, B]), type(E, A, ENV, NE1), env_add(NE1, NE, X, B).
+type(lambda(X, E), T, ENV, _) :- env_add(ENV, NE1, X, ARGTYPE), type(E, BODYTYPE, NE1, _), =(T, [func, ARGTYPE, BODYTYPE]).
