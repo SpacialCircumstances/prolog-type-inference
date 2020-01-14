199042e6b4cb5292ec706475ab55b55c5baf264d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-01-14 19:23:02 +0100

    Get rid of unused env out argument

--- typing.pl
+++ typing.pl
@@ -2,14 +2,14 @@ use_module(library(assoc)).
 use_module(library(plunit)).
 env_add(OLDENV, NEWENV, X, T) :- put_assoc(X, OLDENV, T, NEWENV).
 env_contains(ENV, X, T) :- get_assoc(X, ENV, T).
-type(const(_), int, _, _).
-type(var(X), T, ENV, _) :- env_contains(ENV, X, T).
-type(app(F, A), T, ENV, NE) :- type(F, [func, X, T], ENV, NE1), type(A, X, NE1, NE).
-type(lambda(X, E), T, ENV, _) :- env_add(ENV, NE1, X, ARGTYPE), type(E, BODYTYPE, NE1, _), =(T, [func, ARGTYPE, BODYTYPE]).
-type(let(X, VE, BE), T, ENV, _) :- type(VE, VT, ENV, _), env_add(ENV, NE1, X, VT), type(BE, T, NE1, _).
+type(const(_), int, _).
+type(var(X), T, ENV) :- env_contains(ENV, X, T).
+type(app(F, A), T, ENV) :- type(F, [func, X, T], ENV), type(A, X, ENV).
+type(lambda(X, E), T, ENV) :- env_add(ENV, NEW_ENV, X, ARGTYPE), type(E, BODYTYPE, NEW_ENV), =(T, [func, ARGTYPE, BODYTYPE]).
+type(let(X, VE, BE), T, ENV) :- type(VE, VT, ENV), env_add(ENV, NEW_ENV, X, VT), type(BE, T, NEW_ENV).
 
 :- begin_tests(typing).
-test(const_type) :- type(const(2), int, _, _).
-test(simple_var) :- list_to_assoc([ x - int ], E), type(var(x), int, E, _).
-test(id_function) :- once(type(lambda(x, var(x)), [func, T1, T2], _, _)), =(T1, T2).
+test(const_type) :- type(const(2), int, _).
+test(simple_var) :- list_to_assoc([ x - int ], E), type(var(x), int, E).
+test(id_function) :- once(type(lambda(x, var(x)), [func, T1, T2], _)), =(T1, T2).
 :- end_tests(typing).
