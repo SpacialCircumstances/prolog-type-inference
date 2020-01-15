cb151f4c8bdfca0bba25f112b60abf04e8c7d38d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-01-15 18:51:14 +0100

    Add type inference for if expressions

--- typing.pl
+++ typing.pl
@@ -8,6 +8,7 @@ type(var(X), T, ENV) :- env_contains(ENV, X, T).
 type(app(F, A), T, ENV) :- type(F, [func, X, T], ENV), type(A, X, ENV).
 type(lambda(X, E), T, ENV) :- env_add(ENV, NEW_ENV, X, ARGTYPE), type(E, BODYTYPE, NEW_ENV), =(T, [func, ARGTYPE, BODYTYPE]).
 type(let(X, VE, BE), T, ENV) :- env_add(ENV, NEW_ENV, X, VT), type(VE, VT, ENV), type(BE, T, NEW_ENV).
+type(if(COND, IF, ELSE), T, ENV) :- type(COND, bool, ENV), type(IF, T, ENV), type(ELSE, T, ENV).
 
 :- begin_tests(typing).
 test(const_type) :- type(int_const(2), int, _).
@@ -16,4 +17,5 @@ test(id_function) :- once(type(lambda(x, var(x)), [func, T1, T2], _)), =(T1, T2)
 test(let_with_id) :- once(type(let(id, lambda(x, var(x)), app(var(id), int_const(1))), int, _)).
 test(variable_shadowing) :- once(type(lambda(x, lambda(x, var(x))), T, _)), =(T, [func, _, [func, A, A]]).
 test(simple_application) :- once(type(app(lambda(x, var(x)), bool_const(true)), bool, _)).
+test(basic_if) :- once(type(if(bool_const(true), int_const(1), int_const(2)), int, _)).
 :- end_tests(typing).
