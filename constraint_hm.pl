5e6a72d50474df139460b36b61fabe5e27df91a8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-01-17 18:13:33 +0100

    Fix generalization for atomic types

--- constraint_hm.pl
+++ constraint_hm.pl
@@ -1,5 +1,6 @@
 eq(A, A).
 inst(var(X), G) :- dif(X, G).
 inst(A, A).
-generalized(A, GEN) :- maplist(inst, A, GEN).
+generalized(is_list(A), GEN) :- maplist(inst, A, GEN).
+generalized(X, X).
 eq_gen(A, B, _) :- generalized(B, GENERALIZED), display(GENERALIZED), eq(A, GENERALIZED).
